a412f99c2901a9fbb0d9a7685718158a7406f94d
Andirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2022-04-09 15:13:43 +0300

    Add files via upload

---
+++ Odd or even .py
@@ -0,0 +1,8 @@
+#copyright of this code have Abdirizak abdullahi hussein 
+# odd or even checker
+num = int(input("Enter a number:"))
+
+if int(num) % 2 == 0:
+    print(int(num) , "it's even number")
+else:
+    print(int(num) , "it's odd number")
